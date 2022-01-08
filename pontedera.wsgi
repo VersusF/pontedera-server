fbffbc86c6c08d68ea1e1c22e89707ac7463540e
BertiMarco <ber.bertino@gmail.com>
2022-01-08 15:01:22 +0100

    wake on lan, remote file copy

--- pontedera.wsgi
+++ pontedera.wsgi
@@ -2,4 +2,3 @@
 import sys
 sys.path.insert(0, "/var/www/pontedera/")
 from pontedera import app as application
-
--- routes/printer.py
+++ routes/printer.py
@@ -1,11 +1,22 @@
+import os
 from flask import Blueprint, render_template, request, redirect
 from dotenv import load_dotenv
 from bcrypt import checkpw
-import os
 
 load_dotenv()
 
 printer = Blueprint("printer", __name__, template_folder="../templates")
+SERVER_LOCAL_IP = '192.168.178.69'
+COD_MAC = os.environ.get("COD_MAC")
+
+
+def wake_on_lan():
+    # Lanciare pacchetto
+    os.system('wakeonlan {} > /dev/null'.format(COD_MAC))
+    # TODO Chiamare qui copy_file_to server? Bisogna aspettarere che si accenda il server.
+
+def copy_file_to_server(file_path):
+    os.system('scp {} cod@{}:/home/cod/to_print > /dev/null'.format(file_path, SERVER_LOCAL_IP))
 
 
 @printer.route("", methods=["GET"])
@@ -22,8 +33,15 @@ def submit():
     if checkpw(password, pwd_hash):
         copies = request.form['copies']
         file = request.files['file_path']
-        file.save("./tmp/" + file.filename)
+        file_save_path = "./tmp/" +file.filename
+        file.save(file_save_path)
         print(password, copies, file)
+        command = 'timeout 0.2s ping {} -c 1 > /dev/null'.format(SERVER_LOCAL_IP)
+        ping_res = os.system(command)
+        if ping_res == 0:
+            copy_file_to_server(file_save_path)
+        else:
+            wake_on_lan()
     else:
         print("Password sbagliata")
     return redirect("/printer")
