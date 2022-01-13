dd70908282a6d7059daff092bca85db107f0072c
Filippo Contro <controfilippo8@gmail.com>
2022-01-14 00:49:06 +0100

    Structure for job lists

--- src/static/printer/printer.js
+++ src/static/printer/printer.js
@@ -16,18 +16,60 @@ async function login() {
             password: pwd,
         });
         token = data.token;
-        toastSuccess("Benvenuto");
-        afterLogin();
+        afterLogin().then(() => {
+            toastSuccess("Benvenuto");
+        }).catch(() => {
+            toastError("Errore interno");
+        });
     } catch (error) {
         toastError("Password errata");
     }
 }
 
-function afterLogin() {
+async function afterLogin() {
     const loginDiv = document.getElementById("login");
     const mainDiv = document.getElementById("main");
     loginDiv.style.display = "none";
     mainDiv.style.display = "block";
+
+    const baseurl = document.location.origin;
+    const queuedRequest = await axios.get(baseurl + "/printer/queued-jobs", {
+        headers: {
+            "auth": token
+        }
+    });
+    addJobsToTable("queued-jobs", queuedRequest.data.jobs);
+
+    const printedRequest = await axios.get(baseurl + "/printer/printed-jobs", {
+        headers: {
+            "auth": token
+        }
+    });
+    addJobsToTable("printed-jobs", printedRequest.data.jobs);
+}
+
+/**
+ * @param {string} tableId 
+ * @param {{ timestamp: number, filename: string, copies: number }[]} jobs 
+ */
+function addJobsToTable(tableId, jobs) {
+    const queuedTable = document.getElementById(tableId);
+    for (const j of jobs) {
+        const row = document.createElement("tr");
+        const dateCell = document.createElement("td");
+        dateCell.innerHTML = j.timestamp;
+        row.appendChild(dateCell);
+
+        const nameCell = document.createElement("td");
+        nameCell.innerHTML = j.filename;
+        row.appendChild(nameCell);
+
+        const copiesCell = document.createElement("td");
+        copiesCell.innerHTML = j.copies;
+        row.appendChild(copiesCell);
+
+        queuedTable.appendChild(row);
+    }
 }
 
 /**
