90d03127aeec2806d219bdc5cc96619850556cd7
Filippo Contro <controfilippo8@gmail.com>
2022-01-21 22:50:38 +0100

    Improve pwdhash prints

--- src/utils/pwdhash.py
+++ src/utils/pwdhash.py
@@ -7,8 +7,10 @@ def main():
     salt = bcrypt.gensalt()
     pwd_hash = bcrypt.hashpw(pwd.encode("utf8"), salt)
     print("Password hash")
-    print(pwd_hash)
+    print(pwd_hash.decode("utf8"))
     print("Before putting this in the .env file remember to prefix $ (dollar) with a \ (backslash)")
+    print("Paste this in the .env file")
+    print(pwd_hash.decode("utf8").replace("$", "\\$"))
 
 
 if __name__ == "__main__":
